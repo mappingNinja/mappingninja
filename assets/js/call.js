6d59a3e46ec9a45918d22f6a5c09771b563d65c4
mappingNinja <mappingninja@gmail.com>
2023-03-05 18:52:20 +0530

    files-comment

---
+++ assets/js/call.js
@@ -0,0 +1,10 @@
+const contact = () => {
+    try {
+        return alert("For any Enquiry Please call on given Number")
+    }
+    catch (error) {
+        return alert(error);
+    }
+}
+
+module.exports = {contact}
